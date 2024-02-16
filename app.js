b6080e613ed05452f17e9b02e20b9fc3da570074
deepakkotary <deepakkotary.9166@gmail.com>
2024-02-16 17:50:04 +0530

    upadted

--- app.js
+++ app.js
@@ -43,3 +43,41 @@ window.addEventListener('keyup',()=>{
 //alert("sto")  
 store();
 })
+///////>>>>>>>>>>>
+///////>>>>>>>>>>>
+///////>>>>>>>>>>>
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
+  
+  // Store the event for later use
+  deferredPrompt = event;
+
+  // You can show a custom button or UI to prompt the user to install
+  // For example, display a button and add an event listener to trigger the install prompt
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+        // You can track here that the user added the app to the home screen
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+
+      // Reset the deferredPrompt variable
+      deferredPrompt = null;
+    });
+  });
+});
+
+
+///////>>>>>>>>>>>
